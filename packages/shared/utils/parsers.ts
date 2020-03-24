d8cd6805c05d1c87bbbac9804d65b15e88349dcd
Runi Brockie <RuniBrockie@notonthehighstreet.com>
2020-03-24 15:49:30 +0000

    Add util back but change it to check for null first then truthiness

--- packages/shared/utils/parsers.ts
+++ packages/shared/utils/parsers.ts
@@ -1,2 +1,3 @@
 export const mapObjectToArray = (obj: Record<string, any>) => Object.keys(obj).map(k => obj[k]);
 export const stringifyNumber = (val: number) => `${val}`;
+export const isValueNotNullAndTruthy = (val: number) => val != null && !!val;
