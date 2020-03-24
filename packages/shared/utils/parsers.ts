821249fd7d369f9c23d8aaa8807eefd88bf48ad6
Runi Brockie <RuniBrockie@notonthehighstreet.com>
2020-03-24 15:45:28 +0000

    Remove new util

--- packages/shared/utils/parsers.ts
+++ packages/shared/utils/parsers.ts
@@ -1,3 +1,2 @@
 export const mapObjectToArray = (obj: Record<string, any>) => Object.keys(obj).map(k => obj[k]);
 export const stringifyNumber = (val: number) => `${val}`;
-export const isValueTruthy = (val: number) => !!val;
